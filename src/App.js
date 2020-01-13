67c04311d35271e537fb3b27e4dbcf542b13bfeb
Pierre Eid <pierre.eid2@gmail.com>
2020-01-13 15:19:26 -0500

    under construction final

--- src/App.js
+++ src/App.js
@@ -8,16 +8,10 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Website under construction...<br/>Pierre-Anthony Eid
+          Website under construction...
+          <br/><br/>
+          Pierre-Anthony Eid
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
       </header>
     </div>
   );
