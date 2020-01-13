94f2528db90d61ec4fb1292f9e72d1d46cbdf767
Pierre Eid <pierre.eid2@gmail.com>
2020-01-13 15:10:11 -0500

    Updates

---
+++ precache-manifest.ce4c99a55626d7a9d515d1506ca92d59.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "452f5d759f9dba33c79ac6d17f32b2f7",
+    "url": "/my-website/index.html"
+  },
+  {
+    "revision": "15a0d01ba1d2560eeed6",
+    "url": "/my-website/static/css/main.d1b05096.chunk.css"
+  },
+  {
+    "revision": "6c4f2cbd41e02887ebf5",
+    "url": "/my-website/static/js/2.2e83008a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-website/static/js/2.2e83008a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "15a0d01ba1d2560eeed6",
+    "url": "/my-website/static/js/main.b999984b.chunk.js"
+  },
+  {
+    "revision": "8e993b37c0e1de91381a",
+    "url": "/my-website/static/js/runtime-main.962a4678.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-website/static/media/logo.5d5d9eef.svg"
+  }
+]);
