60546eae18e59e21b7dd218cbfc89401b7fa91cd
Pierre Eid <pierre.eid2@gmail.com>
2020-01-13 15:16:12 -0500

    under construction

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Website under construction...<br>Pierre-Anthony Eid
+          Website under construction...<br/>Pierre-Anthony Eid
         </p>
         <a
           className="App-link"
