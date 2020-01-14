f7404f610ed8497956272f2ca772565c90c20730
Pierre Eid <pierre.eid2@gmail.com>
2020-01-14 12:06:33 -0500

    Updates

---
+++ precache-manifest.5c3a8839480a4cc23884f04a1bbea902.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b109ee7081f53081a209834fbd7e41f6",
+    "url": "/my-website/home/index.html"
+  },
+  {
+    "revision": "064251f63e20742b64eb",
+    "url": "/my-website/home/static/css/main.d2a18ca0.chunk.css"
+  },
+  {
+    "revision": "6c4f2cbd41e02887ebf5",
+    "url": "/my-website/home/static/js/2.2e83008a.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/my-website/home/static/js/2.2e83008a.chunk.js.LICENSE"
+  },
+  {
+    "revision": "064251f63e20742b64eb",
+    "url": "/my-website/home/static/js/main.ab45b1ff.chunk.js"
+  },
+  {
+    "revision": "b90112b285c655b9b100",
+    "url": "/my-website/home/static/js/runtime-main.b2c179f5.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/my-website/home/static/media/logo.5d5d9eef.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/my-website/precache-manifest.f9dac4d2c2c29961ddd42375375e3973.js"
+  "/my-website/home/precache-manifest.5c3a8839480a4cc23884f04a1bbea902.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/my-website/home/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
