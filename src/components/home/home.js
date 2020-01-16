0597fa74fdb5faea945d5cbe81f897c71ecf6a15
Pierre Eid <pierre.eid2@gmail.com>
2020-01-15 21:19:06 -0500

    some style changes to the nav

--- src/components/home/home.js
+++ src/components/home/home.js
@@ -2,19 +2,21 @@ import React from 'react';
 import './home.css';
 
 class Home extends React.Component {
-  
+
   render() {
     return(
       <div className="home">
         <div className="home-content">
           <h3>Pierre-Anthony Eid</h3>
-          <ul className="nav">
-            <li onClick={this.changeView}>About |</li>
-            <li> Personal Blog |</li>
-            <li> Resume |</li>
-            <li> Github |</li>
-            <li> Contact Me</li>
-          </ul>
+          <div className="nav-container">
+            <table className="nav-table">
+              <th>|About</th>
+              <th>|Personal Blog</th>
+              <th>|Resume</th>
+              <th>|Github</th>
+              <th>|Contact Me</th>
+            </table>
+          </div>
         </div>
       </div>
     );
